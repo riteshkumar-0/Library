85cc92b099d5fa0bf66b50eba946f0dea589be3e
riteshkumar-0 <riteshkumar853892@gmail.com>
2025-01-18 12:41:41 +0530

    clean

--- Frontend/src/context/AuthProvider.jsx
+++ Frontend/src/context/AuthProvider.jsx
@@ -13,30 +13,3 @@ export default function AuthProvider({ children }) {
   );
 }
 export const useAuth = () => useContext(AuthContext);
-
-// import React, { createContext, useContext, useState } from "react";
-
-// export const AuthContext = createContext();
-
-// export default function AuthProvider({ children }) {
-//   const initialAuthUser = localStorage.getItem("Users");
-//   const [authUser, setAuthUser] = useState(initialAuthUser ? JSON.parse(initialAuthUser) : undefined);
-
-//   const login = (user) => {
-//     localStorage.setItem("Users", JSON.stringify(user));
-//     setAuthUser(user);
-//   };
-
-//   const logout = () => {
-//     localStorage.removeItem("Users");
-//     setAuthUser(undefined);
-//   };
-
-//   return (
-//     <AuthContext.Provider value={{ authUser, login, logout }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// }
-
-// export const useAuth = () => useContext(AuthContext);
